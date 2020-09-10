e50ebf206f340e207cebd87b8f71f1ff358fa782
WevertonCintra <wevertoncintra@outlook.com>
2020-09-10 12:27:22 -0300

    add new module theme-light-dark

---
+++ theme-light-dark/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+import usePersistedState from './utils/usePersistedState';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }, [theme.title, setTheme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
